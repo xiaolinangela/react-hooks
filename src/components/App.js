7ec86ebf94a15c8ce400dafbb1d782e081473c58
Angela Zhu <xiaolinangela@gmail.com>
2020-03-30 21:28:12 -0700

    code reuse with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import ResouceList from "./ResourceList";
+import UserList from "./UserList";
 
 const App = () => {
   const [resource, setResource] = useState("posts");
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource("posts")}>Posts</button>
         <button onClick={() => setResource("todos")}>Todos</button>
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,27 +1,39 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import useResources from "./useResources";
+
+// const useResources = resource => {
+//   const [resources, setResources] = useState([]);
+//   const fetchResource = async resource => {
+//     const response = await axios.get(
+//       `http://jsonplaceholder.typicode.com/${resource}`
+//     );
+//     setResources(response.data);
+//   };
+//   useEffect(() => {
+//     fetchResource(resource);
+//   }, [resource]);
+
+//   useEffect(() => {
+//     (async resource => {
+//       const response = await axios.get(
+//         `http://jsonplaceholder.typicode.com/${resource}`
+//       );
+//       setResources(response.data);
+//     })(resource);
+//   }, [resource]);
+
+//   return resources;
+// };
 
 const ResourceList = ({ resource }) => {
-  const [resources, setResources] = useState([]);
-  //   const fetchResource = async resource => {
-  //     const response = await axios.get(
-  //       `http://jsonplaceholder.typicode.com/${resource}`
-  //     );
-  //     setResources(response.data);
-  //   };
-  //   useEffect(() => {
-  //     fetchResource(resource);
-  //   }, [resource]);
-
-  useEffect(() => {
-    (async resource => {
-      const response = await axios.get(
-        `http://jsonplaceholder.typicode.com/${resource}`
-      );
-      setResources(response.data);
-    })(resource);
-  }, [resource]);
-  return <div>{resources.length}</div>;
+  const resources = useResources(resource);
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default ResourceList;
